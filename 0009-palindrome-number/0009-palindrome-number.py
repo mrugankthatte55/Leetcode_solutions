84399c95cca895b0dd64301b51bb8714a3abd6da
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-03-25 19:04:41 -0700

    Time: 69 ms (8.33%), Space: 16.5 MB (97.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,7 +2,9 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x<0:
             return False
-        a = [int(d) for d in str(x)]
+        a = []
+        for i in str(x):
+            a.append(int(i))
         if(a==a[::-1]):
             return True
         return False
