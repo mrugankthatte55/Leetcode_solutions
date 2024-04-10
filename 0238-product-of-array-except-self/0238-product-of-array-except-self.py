ae6f5fe7ddb14f19d9721c11e753c38be89aa29c
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-04-09 17:54:41 -0700

    Time: 352 ms (5.21%), Space: 46.1 MB (6.57%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n=len(nums)
@@ -8,7 +9,8 @@ class Solution:
         for i in range(1,n):
             prodLeft[i]=prodLeft[i-1]*nums[i-1]
             prodRight[n-i-1]=prodRight[n-i]*nums[n-i]
-        commonProd=[0]*n
-        for i in range(n):
-            commonProd[i]=prodLeft[i]*prodRight[i]
-        return(commonProd)
+        return(np.multiply(prodLeft,prodRight).tolist())
+        # commonProd=[0]*n
+        # for i in range(n):
+        #     commonProd[i]=prodLeft[i]*prodRight[i]
+        # return(commonProd)
