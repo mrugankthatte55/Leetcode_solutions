afa3649ae4676a3d0a26d82d5b33cd24e695becf
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-04-12 22:16:17 -0700

    Time: 213 ms (79.49%), Space: 17.8 MB (76.17%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -0,0 +1,21 @@
+class Solution:
+    def leetcode84(self, heights):
+        stack = [-1]
+        maxarea = 0
+        for i in range(len(heights)):
+            while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
+                maxarea = max(maxarea, heights[stack.pop()] * (i - stack[-1] - 1))
+            stack.append(i)
+        while stack[-1] != -1:
+            maxarea = max(maxarea, heights[stack.pop()] * (len(heights) - stack[-1] - 1))
+        return maxarea
+
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix: return 0
+        maxarea = 0
+        dp = [0] * len(matrix[0])
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                dp[j] = dp[j] + 1 if matrix[i][j] == '1' else 0
+            maxarea = max(maxarea, self.leetcode84(dp))
+        return maxarea
