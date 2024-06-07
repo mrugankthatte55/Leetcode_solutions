7f57175ff42dcef9e8b2664bae141907fd756490
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-06-07 00:08:20 -0700

    Time: 210 ms (30.54%), Space: 24.9 MB (94.66%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,6 +1,5 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        dictionary=set(dictionary)
         s=list(sentence.split(" "))
         for i in range(len(s)):
             for j in dictionary:
