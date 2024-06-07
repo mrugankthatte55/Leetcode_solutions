c38b111621ab1905ffc7acf1b34e0aeced4728eb
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-06-06 23:03:28 -0700

    Time: 222 ms (27.19%), Space: 25.2 MB (66.91%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,10 +1,23 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        # dictionary.sort()
+        # s=list(sentence.split(" "))
+        # for i in range(len(s)):
+        #     # f=0
+        #     for j in dictionary:
+        #         # if s[i][:len(j)] == j and f == 0:
+        #         if s[i][:len(j)] == j:
+        #             s[i]=j
+        #             # f=1
+        # listToStr = ' '.join([str(c) for c in s])
+        # return listToStr
+        
         dictionary.sort()
         s=list(sentence.split(" "))
-        for i in range(len(s)):
+        
             # f=0
-            for j in dictionary:
+        for j in dictionary:
+            for i in range(len(s)):
                 # if s[i][:len(j)] == j and f == 0:
                 if s[i][:len(j)] == j:
                     s[i]=j
@@ -18,3 +31,4 @@ class Solution:
 
 
 # can also use string.startswith()
+
