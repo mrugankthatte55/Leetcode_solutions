68ec629bb0aace22ef805de38697fadf86034bc0
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-06-06 23:26:29 -0700

    Time: 210 ms (30.54%), Space: 25.2 MB (66.51%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,30 +1,26 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        # dictionary.sort()
-        # s=list(sentence.split(" "))
-        # for i in range(len(s)):
-        #     # f=0
-        #     for j in dictionary:
-        #         # if s[i][:len(j)] == j and f == 0:
-        #         if s[i][:len(j)] == j:
-        #             s[i]=j
-        #             # f=1
-        # listToStr = ' '.join([str(c) for c in s])
-        # return listToStr
-        
-        d={}
-        for i in dictionary:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
+        dictionary.sort()
         s=list(sentence.split(" "))
-        for j in d:
-            for i in range(len(s)):
+        for i in range(len(s)):
+            for j in dictionary:
                 if s[i][:len(j)] == j:
                     s[i]=j
-        listToStr = " ".join(s)
-        return listToStr
+        return ' '.join([str(c) for c in s])
+        
+        # d={}
+        # for i in dictionary:
+        #     if i in d:
+        #         d[i]+=1
+        #     else:
+        #         d[i]=1
+        # s=list(sentence.split(" "))
+        # for j in d:
+        #     for i in range(len(s)):
+        #         if s[i][:len(j)] == j:
+        #             s[i]=j
+        # listToStr = " ".join(s)
+        # return listToStr
         
 # Gives TLE
 # regex=re.compile('^hello')
