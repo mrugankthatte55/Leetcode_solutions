9d6de010503f7eb8b4b8ccbe190158861496d657
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-06-07 22:39:09 -0700

    Time: 66 ms (77.69%), Space: 17.5 MB (44.63%) - LeetHub

--- 2083-substrings-that-begin-and-end-with-the-same-letter/2083-substrings-that-begin-and-end-with-the-same-letter.py
+++ 2083-substrings-that-begin-and-end-with-the-same-letter/2083-substrings-that-begin-and-end-with-the-same-letter.py
@@ -10,17 +10,18 @@ class Solution:
         # for i in d:
         #     ans+=(((d[i]*d[i]) + d[i])//2)
         # return ans
-        n = len(s)
         answer = 0
-        prefix_count = [0] * 26
+        frequency_count = [0] * 26
 
-        for i in range(len(s)):
-            # Increment the number of times we encountered the current letter so far.
-            prefix_count[ord(s[i]) - ord("a")] += 1
+        # Count the frequency of each character in the string.
+        for ch in s:
+            frequency_count[ord(ch) - ord("a")] += 1
 
-            # Current letter can be paired with all the occurrences of it that
-            # comes before, including itself, to form a valid substring.
-            answer += prefix_count[ord(s[i]) - ord("a")]
+        # Calculate the total number of valid substrings.
+        for current_count in frequency_count:
+            # Using (current_count + 1) choose 2 to calculate valid substrings
+            # for the current letter.
+            answer += ((current_count + 1) * current_count) // 2
 
         return answer
     
