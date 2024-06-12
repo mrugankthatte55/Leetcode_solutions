01ffdf9bae76595728cd2308359ea593328a6414
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-06-11 21:11:36 -0700

    Time: 35 ms (73.49%), Space: 16.4 MB (79.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,18 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # arr=[0,0,0]
-        # for i in range(len(nums)):
-        #     arr[nums[i]]+=1
-        # # print(arr)
-        # for i in range(len(nums)):
-        #     if arr[0]!=0:
-        #         nums[i]=0
-        #         arr[0]-=1
-        #     elif arr[1]!=0:
-        #         nums[i]=1
-        #         arr[1]-=1
-        #     elif arr[2]!=0:
-        #         nums[i]=2
-        #         arr[2]-=1
-        nums.sort()
+        arr=[0,0,0]
+        for i in range(len(nums)):
+            arr[nums[i]]+=1
+        # print(arr)
+        for i in range(len(nums)):
+            if arr[0]!=0:
+                nums[i]=0
+                arr[0]-=1
+            elif arr[1]!=0:
+                nums[i]=1
+                arr[1]-=1
+            elif arr[2]!=0:
+                nums[i]=2
+                arr[2]-=1
