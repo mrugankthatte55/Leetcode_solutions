21f34b90c85318936d5100fba5f3b4e03b5d6530
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-06-21 08:24:42 -0700

    Time: 5248 ms (5.04%), Space: 19 MB (38.37%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -2,46 +2,65 @@ class Solution:
     def maxSatisfied(
         self, customers: List[int], grumpy: List[int], minutes: int
     ) -> int:
-        l=[0]*len(customers)
-        r=[0]*len(customers)
-        if grumpy[0]==0:
-            l[0]=customers[0]
-        else:
-            l[0]=0
-        for i in range(1,len(customers)):
+        ans=0
+        for i in range(len(customers)):
             if not grumpy[i]:
-                l[i]=l[i-1]+customers[i]
-            else:
-                l[i]=l[i-1]
-        # print(l)
-        
-        if not grumpy[-1]:
-            r[-1]=customers[-1]
-        else:
-            r[-1]=0
-        for i in range(len(customers)-2,-1,-1):
-            if not grumpy[i]:
-                r[i]=r[i+1]+customers[i]
-            else:
-                r[i]=r[i+1]
-        # print(r)
+                ans+=customers[i]
         left=0
         right=minutes-1
-        ans=0
-        while left<=len(grumpy)-minutes and right<len(grumpy):
-            s=0
+        s=0
+        superans=ans
+        while right<len(customers):
+            s=ans
             for i in range(left,right+1):
-                s+=customers[i]
-                # print(customers[i])
-            # print("-------")
-            if left>0:
-                s+=l[left-1]
-            if right<len(grumpy)-1:
-                s+=r[right+1]
-            if s>ans:
-                ans=s
-            left+=1
+                if grumpy[i]:
+                    s+=customers[i]
+            if s>superans:
+                superans=s
             right+=1
+            left+=1
+        return superans
+#         l=[0]*len(customers)
+#         r=[0]*len(customers)
+#         if grumpy[0]==0:
+#             l[0]=customers[0]
+#         else:
+#             l[0]=0
+#         for i in range(1,len(customers)):
+#             if not grumpy[i]:
+#                 l[i]=l[i-1]+customers[i]
+#             else:
+#                 l[i]=l[i-1]
+#         # print(l)
+        
+#         if not grumpy[-1]:
+#             r[-1]=customers[-1]
+#         else:
+#             r[-1]=0
+#         for i in range(len(customers)-2,-1,-1):
+#             if not grumpy[i]:
+#                 r[i]=r[i+1]+customers[i]
+#             else:
+#                 r[i]=r[i+1]
+#         # print(r)
+#         left=0
+#         right=minutes-1
+#         ans=0
+#         while left<=len(grumpy)-minutes and right<len(grumpy):
+#             s=0
+#             for i in range(left,right+1):
+#                 s+=customers[i]
+#                 # print(customers[i])
+#             # print("-------")
+#             if left>0:
+#                 s+=l[left-1]
+#             if right<len(grumpy)-1:
+#                 s+=r[right+1]
+#             if s>ans:
+#                 ans=s
+#             left+=1
+#             right+=1
             
-        return ans
+#         return ans
+        
         
