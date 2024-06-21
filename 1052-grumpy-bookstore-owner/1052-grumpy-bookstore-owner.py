22d0e2ae3aa48ecc414153cbe8177a2fa42c2c35
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-06-21 02:38:19 -0700

    Time: 4082 ms (5.38%), Space: 18.9 MB (64.93%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -2,23 +2,46 @@ class Solution:
     def maxSatisfied(
         self, customers: List[int], grumpy: List[int], minutes: int
     ) -> int:
-        n = len(customers)
-        unrealized_customers = 0
-        for i in range(minutes):
-            unrealized_customers += customers[i] * grumpy[i]
-        max_unrealized_customers = unrealized_customers
-
-        for i in range(minutes, n):
-            unrealized_customers += customers[i] * grumpy[i]
-            unrealized_customers -= customers[i - minutes] * grumpy[i - minutes]
-
-            max_unrealized_customers = max(
-                max_unrealized_customers, unrealized_customers
-            )
-
-        total_customers = max_unrealized_customers
-
-        for i in range(n):
-            total_customers += customers[i] * (1 - grumpy[i])
-
-        return total_customers
+        l=[0]*len(customers)
+        r=[0]*len(customers)
+        if grumpy[0]==0:
+            l[0]=customers[0]
+        else:
+            l[0]=0
+        for i in range(1,len(customers)):
+            if not grumpy[i]:
+                l[i]=l[i-1]+customers[i]
+            else:
+                l[i]=l[i-1]
+        # print(l)
+        
+        if not grumpy[-1]:
+            r[-1]=customers[-1]
+        else:
+            r[-1]=0
+        for i in range(len(customers)-2,-1,-1):
+            if not grumpy[i]:
+                r[i]=r[i+1]+customers[i]
+            else:
+                r[i]=r[i+1]
+        # print(r)
+        left=0
+        right=minutes-1
+        ans=0
+        while left<=len(grumpy)-minutes and right<len(grumpy):
+            s=0
+            for i in range(left,right+1):
+                s+=customers[i]
+                # print(customers[i])
+            # print("-------")
+            if left>0:
+                s+=l[left-1]
+            if right<len(grumpy)-1:
+                s+=r[right+1]
+            if s>ans:
+                ans=s
+            left+=1
+            right+=1
+            
+        return ans
+        
