2f37c0d7f3d706349aeaebe6abccf2d0a3ed46f7
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-07-09 22:08:53 -0700

    Time: 48 ms (63.33%), Space: 16.9 MB (11.07%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,12 +1,12 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        st=[]
+        st=0
         for i in logs:
             if i=="./":
                 continue
-            if i=="../" and len(st)!=0:
-                st.pop()
+            if i=="../" and st!=0:
+                st-=1
             elif i!="../":
-                st.append(i)
+                st+=1
             print(st)
-        return len(st)
+        return st
