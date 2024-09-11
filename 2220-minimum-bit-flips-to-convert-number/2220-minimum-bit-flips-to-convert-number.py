875d832ee809e7d3ba98df3471f9558888dfdafb
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2024-09-10 18:43:45 -0700

    Time: 38 ms (37.69%), Space: 16.5 MB (26.96%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,6 +1,6 @@
-class Solution:
-    def minBitFlips(self, start: int, goal: int) -> int:
-        return (start ^ goal).bit_count()
+# class Solution:
+#     def minBitFlips(self, start: int, goal: int) -> int:
+#         return (start ^ goal).bit_count()
 
 
 
@@ -15,32 +15,32 @@ class Solution:
     
     
     
-# class Solution:
-#     def minBitFlips(self, start: int, goal: int) -> int:
-#         def getBinaryList(x):
-#             res = []
-#             bit = 1
-#             while x > 0:
-#                 if x & 1:
-#                     res.append(1)
-#                 else:
-#                     res.append(0)
-#                 x = x >> 1
-#                 bit = bit << 1
-#             # res.reverse()
-#             return res
-#         start=getBinaryList(start)
-#         goal=getBinaryList(goal)
-#         if len(start)>len(goal):
-#             for i in range(len(start)-len(goal)):
-#                 goal.append(0)
-#         elif len(start)<len(goal):
-#             for i in range(len(goal)-len(start)):
-#                 start.append(0)
-#         start.reverse()
-#         goal.reverse()
-#         ans=0
-#         for i in range(len(start)):
-#             if goal[i]!=start[i]:
-#                 ans+=1
-#         return ans
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        def getBinaryList(x):
+            res = []
+            bit = 1
+            while x > 0:
+                if x & 1:
+                    res.append(1)
+                else:
+                    res.append(0)
+                x = x >> 1
+                bit = bit << 1
+            # res.reverse()
+            return res
+        start=getBinaryList(start)
+        goal=getBinaryList(goal)
+        if len(start)>len(goal):
+            for i in range(len(start)-len(goal)):
+                goal.append(0)
+        elif len(start)<len(goal):
+            for i in range(len(goal)-len(start)):
+                start.append(0)
+        start.reverse()
+        goal.reverse()
+        ans=0
+        for i in range(len(start)):
+            if goal[i]!=start[i]:
+                ans+=1
+        return ans
