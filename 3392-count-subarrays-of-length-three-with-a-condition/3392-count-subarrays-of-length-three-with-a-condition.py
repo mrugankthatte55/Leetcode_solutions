5b7c648f2e1a6855da349b434e033c6e6371eeb6
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2025-04-28 00:16:32 -0700

    Time: 7 ms (58.7%), Space: 17.8 MB (74.63%) - LeetHub

---
+++ 3392-count-subarrays-of-length-three-with-a-condition/3392-count-subarrays-of-length-three-with-a-condition.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        ans=0
+        for i in range(0,len(nums)-2):
+            if (nums[i]+nums[i+2])*2==nums[i+1]:
+                ans+=1
+        return ans
