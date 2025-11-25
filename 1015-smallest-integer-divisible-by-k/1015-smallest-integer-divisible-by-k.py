917cf62cce36f628bfe2f2f7dbfac9bed3390a98
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2025-11-25 11:58:33 -0700

    Time: 1006 ms (11.98%), Space: 18.1 MB (26.46%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
@@ -3,12 +3,10 @@ class Solution:
         if k%2==0 or k%5==0:
             return -1
         n=1
-        if n%k==0:
-            return 1
-        for i in range (2,k+1):
-            n*=10
-            n+=1
+        for i in range (1,k+1):
             if n%k==0:
                 return i
+            n*=10
+            n+=1
         return -1
         
