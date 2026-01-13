f64cceb539a72a943738855c60a2e317f9d21c6c
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2026-01-13 02:02:52 -0800

    Time: 144 ms (10.03%), Space: 33 MB (8.41%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.py
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.py
@@ -10,15 +10,15 @@ class Solution:
                     return False
             return True
         for i in range(n):
-            if nums[i][i]>ans:
-                if isPrime(nums[i][i]):
-                    ans=nums[i][i]
-            if nums[i][n-1-i]>ans:
-                if isPrime(nums[i][n-1-i]):
-                    ans=nums[i][n-1-i]
+            # if nums[i][i]>ans:
+            #     if isPrime(nums[i][i]):
+            #         ans=nums[i][i]
+            # if nums[i][n-1-i]>ans:
+            #     if isPrime(nums[i][n-1-i]):
+            #         ans=nums[i][n-1-i]
 
-            # if isPrime(nums[i][i]):
-            #     ans=max(ans,nums[i][i])
-            # if isPrime(nums[i][n-i-1]):
-            #     ans=max(ans,nums[i][n-1-i])
+            if isPrime(nums[i][i]):
+                ans=max(ans,nums[i][i])
+            if isPrime(nums[i][n-i-1]):
+                ans=max(ans,nums[i][n-1-i])
         return ans
