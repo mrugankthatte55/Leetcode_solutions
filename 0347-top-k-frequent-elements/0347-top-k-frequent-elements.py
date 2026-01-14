b8510ee066c7cc942d42324e55068655efd51021
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2026-01-14 02:31:42 -0800

    Time: 7 ms (54.09%), Space: 22.9 MB (23.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,44 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        buckets = [[] for i in range(len(nums) + 1)]
+        if len(nums)==k:
+            return nums
         d={}
-        for n in nums:
-            d[n] = 1 + d.get(n, 0)
-        for n, c in d.items():
-            buckets[c].append(n)
-        ans = []
-        for i in range(len(buckets) - 1, 0, -1):
-            for n in buckets[i]:
-                ans.append(n)
-                if len(ans) == k:
-                    return ans
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        return heapq.nlargest(k,d.keys(),key=d.get)
+        # ans=[]
+        # v=list(d.values())
+        # v.sort(reverse='True')
+        # print(v)
+        # for i in range(len(v)-k):
+        #     v.pop()
+        # t=1
+        # print(v)
+        # for i in range(len(nums)-1):
+
+        #     if nums[i]==nums[i+1]:
+        #         t+=1
+        #     else:
+        #         if t in v:
+        #             ans.append(nums[i])
+        #             t=1
+        #             print(t)
+        # if t in v:
+        #     ans.append(nums[i])
+        #     t=1
+        #     print(t)
+        # d1={}
+        # for i in d:
+        #     d1[d[i]]=i
+        # print(d)
+        # print(d1)
+        # f=list(d.values())
+        # f.sort(reverse='True')
+        # print(f)
+        # ans=[]
+        # for i in range(k):
+        #     ans.append(d1[f[i]])
+        # return ans
