f432be94319d3e1aeb982545d71a0ce8f0e16309
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2026-01-24 03:34:34 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
@@ -3,7 +3,12 @@ class Solution:
         nums.sort()
         ans=0
         n=len(nums)
+        l=(n//2)-1
+        r=n//2
         for i in range(n//2):
-            ans=max(ans,nums[i]+nums[n-i-1])
+            if nums[l-i]+nums[r+i]>=ans:
+                ans=max(ans,nums[l-i]+nums[r+i])
+            else:
+                return ans
         return ans
 
