db5a05110e99ffff584b89bbb46f182d6b1d3bbd
Mrugank Thatte <80316972+mrugankthatte55@users.noreply.github.com>
2026-03-05 02:48:57 -0800

    Time: 15 ms (18.13%), Space: 19.5 MB (7.57%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string-03-05-2026-02-48-57.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        def ops(sres):
+            ans=0
+            for i in range(len(s)):
+                if sres[i]!=s[i]:
+                    ans+=1
+            return ans
+        s1=""
+        s2=""
+        for i in range(len(s)):
+            if i%2==0:
+                s1+='0'
+                s2+='1'
+            else:
+                s1+='1'
+                s2+='0'
+        res=ops(s1)
+        return min(res,len(s)-res)    
+        
+        
